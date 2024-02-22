4d2328d42f8497910c1e6c88197869e8ab891529
Djordje Arandjelovic <djordjearandjelovic993@gmail.com>
2024-02-22 12:11:21 +0100

    toastify

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import styled from "styled-components";
 import { useState, useEffect } from "react";
 import { FormRow, Logo } from "../components";
+import { toast } from "react-toastify";
 
 const initialState = {
 	name: "",
@@ -16,14 +17,14 @@ const Register = () => {
 	const handleChange = (e) => {
 		const name = e.target.name;
 		const value = e.target.value;
-		// console.log(`${name}: ${value}`);
+		console.log(`${name}: ${value}`);
 		setValues({ ...values, [name]: value });
 	};
 	const handleSubmit = (e) => {
 		e.preventDefault();
 		const { name, email, password, isMember } = values;
 		if (!email || !password || (!isMember && !name)) {
-			console.log("Please fill out all fields...");
+			toast.error("Please fill out all fields");
 		}
 	};
 	const toggleMember = () => {
